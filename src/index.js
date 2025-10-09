917fe37ce2468ea79823d5f1d2d335843fcaa3c6
Raghav Arora <100126023+Raghavarora09@users.noreply.github.com>
2025-10-09 20:22:54 +0530

    Revert "Try/token"

--- src/index.js
+++ src/index.js
@@ -38,8 +38,6 @@ root.render(
           <Route path="/checkout" element={<Checkout />} />
           <Route path="*" element={<PageNotFound />} />
           <Route path="/product/*" element={<PageNotFound />} />
-          <Route path="/random/*" element={<PageNotFound />} />
-
         </Routes>
       </Provider>
     </ScrollToTop>
